1a16f7459181827508aa719442c1d018553f4853
lgapontes <lgapontes@gmail.com>
2018-04-09 23:33:37 -0300

    Sessoes SPA e publicacoes

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$('#nav-principal').on('click',function(event) {	
+	esconderTodos(function() {
+		$('#main-principal').css('display','block');
+	});		
+});
+$('#nav-curriculo').on('click',function(event) {	
+	esconderTodos(function() {
+		$('#main-curriculo').css('display','block');
+	});		
+});
+$('#nav-sistemas').on('click',function(event) {	
+	esconderTodos(function() {
+		$('#main-sistemas').css('display','block');
+	});		
+});
+$('#nav-publicacoes').on('click',function(event) {	
+	esconderTodos(function() {
+		$('#main-publicacoes').css('display','block');
+	});		
+});
+$('#nav-jogos').on('click',function(event) {	
+	esconderTodos(function() {
+		$('#main-jogos').css('display','block');
+	});
+});
+
+function esconderTodos(callback) {
+	$( "main" ).each(function() {
+		$(this).css('display','none');	
+	});	
+	callback();
+	 $('.navbar-toggler-icon').click();
+}
