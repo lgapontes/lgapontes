cefe6546c7fa5951d6904a3c0bc1c49e3e9d7e34
lgapontes <lgapontes@gmail.com>
2018-04-11 22:14:49 -0300

    site pronto

--- js/script.js
+++ js/script.js
@@ -1,38 +1,74 @@
+/* bookmarkable */
+var pagina = '#';
+var hash = window.location.hash;
+if (hash && hash.length > 0) {
+	pagina = hash;
+}
+
+if (pagina === '#principal') {
+	esconderTodos(false,function() {		
+		$('#main-principal').css('display','block');
+	});
+} else if (pagina === '#curriculo') {
+	esconderTodos(false,function() {
+		$('#main-curriculo').css('display','block');
+	});
+} else if (pagina === '#sistemas') {
+	esconderTodos(false,function() {
+		$('#main-sistemas').css('display','block');
+	});
+} else if (pagina === '#publicacoes') {
+	esconderTodos(false,function() {
+		$('#main-publicacoes').css('display','block');
+	});
+} else if (pagina === '#linux') {
+	esconderTodos(false,function() {
+		$('#main-linux').css('display','block');
+	});
+} else if (pagina === '#jogos') {
+	esconderTodos(false,function() {
+		$('#main-jogos').css('display','block');
+	});
+} 
+
+/* Nav */
 $('#nav-principal').on('click',function(event) {	
-	esconderTodos(function() {
+	esconderTodos(true,function() {
 		$('#main-principal').css('display','block');
 	});		
 });
 $('#nav-curriculo').on('click',function(event) {	
-	esconderTodos(function() {
+	esconderTodos(true,function() {
 		$('#main-curriculo').css('display','block');
 	});		
 });
 $('#nav-sistemas').on('click',function(event) {	
-	esconderTodos(function() {
+	esconderTodos(true,function() {
 		$('#main-sistemas').css('display','block');
 	});		
 });
 $('#nav-publicacoes').on('click',function(event) {	
-	esconderTodos(function() {
+	esconderTodos(true,function() {
 		$('#main-publicacoes').css('display','block');
 	});		
 });
 $('#nav-linux').on('click',function(event) {	
-	esconderTodos(function() {
+	esconderTodos(true,function() {
 		$('#main-linux').css('display','block');
 	});
 });
 $('#nav-jogos').on('click',function(event) {	
-	esconderTodos(function() {
+	esconderTodos(true,function() {
 		$('#main-jogos').css('display','block');
 	});
 });
 
-function esconderTodos(callback) {
+function esconderTodos(click,callback) {
 	$( "main" ).each(function() {
 		$(this).css('display','none');	
 	});	
 	callback();
-	 $('.navbar-toggler-icon').click();
+	if (click) {
+		$('.navbar-toggler-icon').click();
+	}	
 }
